197b0af75e99e7c457e783fb68aff2d3bd71a0b7
Kevin Gilbert <kgilbert.cmu@gmail.com>
2014-06-03 13:17:01 -0400

    now prints with better format and also counts elements, which will be utilized in future commit with user input

--- XML.py
+++ XML.py
@@ -1,16 +1,22 @@
 import xml.etree.ElementTree as ET
 import urllib2
-DVDs = urllib2.urlopen("http://sciron.cuyahoga.lib.oh.us/feeds/newdvds.xml")
-CDs = urllib2.urlopen("http://cuyahogalibrary.org/feedData/newCDMusic.xml")
+import re
 
+DVD_XML = urllib2.urlopen("http://sciron.cuyahoga.lib.oh.us/feeds/newdvds.xml")
+CD_XML = urllib2.urlopen("http://cuyahogalibrary.org/feedData/newCDMusic.xml")
+
+PAGE_SIZE = 10
 CHANNEL = 0
 
-tree = ET.parse(DVDs)
+tree = ET.parse(DVD_XML)
 root = tree.getroot()[CHANNEL]
 
-for child in root:
-    if child.tag == "item":
-        try:
-            print child[0].text, '\n'
-        except:
-            pass
+DVDs = root.findall("item")
+
+for i in xrange(0,len(DVDs) / PAGE_SIZE):
+    try:
+        for x in xrange(0, PAGE_SIZE):
+            idx = i * PAGE_SIZE + x
+            print "[{0}] {1} \n".format(idx, re.split("\[|/", DVDs[idx][0].text)[0])
+    except UnicodeEncodeError:
+        pass
